fcf4d8724dcdb04e8fd41d51e14888d25d3419b5
lanjing <84336951@qq.com>
2016-06-17 15:36:43 +0800

    add template project use 'pod lib create LJArrayDataSource'

---
+++ Example/Pods/Target Support Files/LJArrayDataSource/LJArrayDataSource-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LJArrayDataSource : NSObject
+@end
+@implementation PodsDummy_LJArrayDataSource
+@end
---
+++ Example/Pods/Target Support Files/LJArrayDataSource/LJArrayDataSource-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LJArrayDataSourceVersionNumber;
+FOUNDATION_EXPORT const unsigned char LJArrayDataSourceVersionString[];
+
